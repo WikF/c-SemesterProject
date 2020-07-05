07f2c572cd4fe9c00cc5c0484d5c12c669c77a4d
Wiktor <wikifed@gmail.com>
2020-07-05 14:54:34 +0200

    commit

---
+++ TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        Board _board;
+        Player[] _players;
+
+        Game ( Board board, Player[] players )
+        {
+            _board = board;
+            _players = players;
+        }
+
+        public static Game init ()
+        {
+            Console.WriteLine( "Welcome to Tic-Tac-Toe" );
+            Board board = new Board();
+            Player[] players;
+
+            int current = 0;
+            bool finish = false;
+            String name;
+            int mark = -1;
+            int gamemode = -1;
+
+            Console.WriteLine( "Choose gamemode: " );
+            Console.WriteLine( "(1) > 1 player" );
+            Console.WriteLine( "(2) > 2 player" );
+            do
+            {
+                try
+                {
+                    gamemode = Int32.Parse( Console.ReadKey( true ).KeyChar.ToString() );
+                }
+                catch ( FormatException ) { };
+            } while ( gamemode < 1 || gamemode > 2 );
+
+            Console.Clear();
+
+            if ( gamemode == 1 )
+            {
+                players = new Player[] { new Human(), new AI() };
+
+                do
+                {
+                    Console.WriteLine( "Player please enter your name:" );
+                    name = Console.ReadLine();
+                } while ( name.Trim().Length == 0 );
+
+                do
+                {
+                    Console.WriteLine( "Player choose mark:\n \t1. X\n\t2. O\n\t3. Y\n\t4. T" );
+                    try
+                    {
+                        mark = Int32.Parse( Console.ReadKey( true ).KeyChar.ToString() );
+                    }
+                    catch ( FormatException ) { }
+                } while ( mark > 4 || mark < 1 );
+
+                players[0].init( name, ( Marks ) mark );
+                mark = mark != 4 ? mark + 1 : 2;
+                players[1].init( "Bot", ( Marks ) mark );
+            }
+            else
+            {
+                players = new Player[] { new Human(), new Human() };
+
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine( "Player {0} please enter your name:", current + 1 );
+                        name = Console.ReadLine();
+                    } while ( name.Trim().Length == 0 );
+
+                    do
+                    {
+                        Console.WriteLine( "Player {0} choose mark:\n \t1. X\n\t2. O\n\t3. Y\n\t4. T", current + 1 );
+                        try
+                        {
+                            mark = Int32.Parse( Console.ReadKey( true ).KeyChar.ToString() );
+                        }
+                        catch ( FormatException ) { }
+                    } while ( mark > 4 || mark < 1 || ( current == 1 ? ( players[0].playerMark == ( Marks )mark ? true : false ) : false ) );
+
+                    players[current].init( name, ( Marks )mark );
+                    current++;
+
+                    if ( current == 2 )
+                        finish = true;
+                } while ( !finish );
+            }
+
+            return new Game( board, players );
+        }
+
+        public void Play ()
+        {
+            int current = -1;
+            while ( true )
+            {                
+                do
+                {
+                    current = ( current + 1 ) % 2;
+                    _players[current].Move( _board );
+                } while ( !_board.isWin( _players[current] ) && !_board.isDraw() );
+
+                Console.WriteLine( "Again? (Y/N)" );
+                bool enter = false;
+                do
+                {
+                    switch ( Console.ReadKey( true ).KeyChar )
+                    {
+                        case 'N':
+                        case 'n':
+                            return;
+                        case 'Y':
+                        case 'y':
+                            _board = new Board();
+                            _players[1].Reset();
+                            enter = true;
+                            break;
+                        default:
+                            break;
+                    }
+                } while ( !enter );
+            }
+        }
+
+        //not in use
+        public void PlayerInfo ()
+        {
+            for ( int i = 0; i < 2; ++i )
+            {
+                Console.WriteLine( "Player {0}:\n\tName: {1}\n\tMark:{2}", i + 1, _players[i].playerName, _players[i].playerMark );
+            }
+        }
+
+        //not in use
+        public void Print ()
+        {
+            _board.Print();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToe
+{
+    abstract class Player
+    {
+        public string playerName { get; protected set; }
+        public Marks playerMark { get; protected set; }
+
+        public abstract void Move ( Board board );
+        public abstract void init ( string Name, Marks mark );
+        public virtual void Reset () {}
+    }
+
+    class Human : Player
+    {
+        public override void init ( string Name, Marks Mark )
+        {
+            playerName = Name;
+            playerMark = Mark;
+        }
+
+        public override void Move ( Board _board )
+        {
+            int row, col;
+            do
+            {
+                row = -1;
+                col = -1;
+                Console.Clear();
+                Console.WriteLine( "Player {0} your turn", playerName );
+                _board.Print();
+                do
+                {
+                    Console.WriteLine( "Row: " );
+                    try
+                    {
+                        row = Int32.Parse( Console.ReadKey().KeyChar.ToString() );
+                        Console.WriteLine();
+                    }
+                    catch ( FormatException ) { }
+                } while ( row < 0 || row > _board.GetSize() );
+                do
+                {
+                    Console.WriteLine( "Col: " );
+                    try
+                    {
+                        col = Int32.Parse( Console.ReadKey().KeyChar.ToString() );
+                        Console.WriteLine();
+                    }
+                    catch ( FormatException ) { }
+                } while ( col < 0 || col > _board.GetSize() );
+            } while ( !_board.SetMark( 3 * row + col, playerMark ) );
+        }
+    }
+
+    class AI : Player
+    {
+        int[] Coefficient = new int[] {
+             1,   0,   1,
+             0,   100, 0,
+             1,   0,   1
+        };
+
+        int AICoefficient = int.MinValue;
+        int HumanCoefficient = int.MaxValue;
+
+        public override void init ( string Name, Marks mark )
+        {
+            playerName = Name;
+            playerMark = mark;
+        }
+
+        public override void Move ( Board board )
+        {
+            if( board.History.Count > 0 )
+            {
+                _HumanCoefficient( board.History.Last() );
+                _possibleWin();
+            }               
+            int position = _MaxValue( board.FreeCell );
+            Coefficient[position] = AICoefficient;
+            _AICoefficient( position );
+
+            board.SetMark( position, playerMark );
+        }
+
+        void print ()
+        {
+            Console.WriteLine();
+            Console.WriteLine( "{0}  0   1   2 ", new string( ' ', 3 ) );
+            for ( int i = 0; i < 3; ++i )
+            {
+                Console.WriteLine( "{0}+---+---+---+", new string( ' ', 3 ) );
+                Console.Write( new string( ' ', 3 ) );
+                for ( int j = 3 * i; j < 3 * i + 3; ++j )
+                {
+                    if ( Coefficient[j] == AICoefficient )
+                        Console.Write( "| A " );
+                    else if ( Coefficient[j] == HumanCoefficient )
+                        Console.Write( "| H " );
+                    else
+                        Console.Write( "| {0} ", Coefficient[j] );
+                }
+                Console.WriteLine( "| {0}", i );
+            }
+            Console.WriteLine( "{0}+---+---+---+", new string( ' ', 3 ) );
+            Console.WriteLine();
+        }
+
+        void _HumanCoefficient ( int pos )
+        {
+            int position;
+            Coefficient[pos] = HumanCoefficient;
+
+            //  Rows change coefficient
+            for ( position = pos % 3; position < 9; position = position + 3 )
+            {
+                if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                    Coefficient[position] -= 10;
+            }
+
+            //  Columns change coefficient
+            int start = pos - pos % 3;
+            for ( position = start; position < start + 3; ++position )
+            {
+                if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                    Coefficient[position] -= 10;
+            }
+
+            // Diagonal change coefficient
+            if ( pos == 0 || pos == 8 )
+            {
+                for ( position = 0; position < 9; position = position + 4 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+            }
+            else if ( pos == 4 )
+            {
+                //  Rows change coefficient
+                for ( position = pos % 3; position < 9; position = position + 3 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+
+                //  Columns change coefficient
+                start = pos - pos % 3;
+                for ( position = start; position < start + 3; ++position )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+                for ( position = 0; position < 9; position = position + 4 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+                for ( position = 2; position <= 6; position = position + 2 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+            }
+            else if ( pos == 2 || pos == 6 )
+            {
+                for ( position = 2; position <= 6; position = position + 2 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] -= 10;
+                }
+            }
+        }
+
+        void _AICoefficient ( int pos )
+        {
+            int position;
+            Coefficient[pos] = AICoefficient;
+
+            //  Rows change coefficient
+            for ( position = pos % 3; position < 9; position = position + 3 )
+            {
+                if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                    Coefficient[position] += 10;
+            }
+
+            //  Columns change coefficient
+            int start = pos - pos % 3;
+            for ( position = start; position < start + 3; ++position )
+            {
+                if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                    Coefficient[position] += 10;
+            }
+
+            // Diagonal change coefficient
+            if ( pos == 0 || pos == 8 )
+            {
+                for ( position = 0; position < 9; position = position + 4 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] += 10;
+                }
+            }
+            else if ( pos == 4 )
+            {
+                for ( position = 0; position < 9; position = position + 4 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] += 10;
+                }
+                for ( position = 2; position <= 6; position = position + 2 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] += 10;
+                }
+            }
+            else if ( pos == 2 || pos == 6 )
+            {
+                for ( position = 2; position <= 6; position = position + 2 )
+                {
+                    if ( Coefficient[position] != HumanCoefficient && Coefficient[position] != AICoefficient )
+                        Coefficient[position] += 10;
+                }
+            }
+        }
+
+        void _possibleWin ()
+        {
+
+            int i = 0, j = 0, count = 0, pos;
+            for ( i = 0; i < 8; ++i )
+            {
+                pos = -1;
+                for ( j = 0; j < 3; ++j )
+                {
+                    if ( Coefficient[Board.WIN_STATE[i, j]] == HumanCoefficient )
+                        count++;
+                    else if ( Coefficient[Board.WIN_STATE[i, j]] != AICoefficient && Coefficient[Board.WIN_STATE[i, j]] != HumanCoefficient )
+                        pos = Board.WIN_STATE[i, j];
+                }
+                if ( count == 2 && pos != -1 )
+                {
+                    Coefficient[pos] = 100;
+                }
+                count = 0;
+            }
+
+            i = 0;
+            j = 0;
+            count = 0;
+            for ( i = 0; i < 8; ++i )
+            {
+                pos = -1;
+                for ( j = 0; j < 3; ++j )
+                {
+                    if ( Coefficient[Board.WIN_STATE[i, j]] == AICoefficient )
+                        count++;
+                    else if ( Coefficient[Board.WIN_STATE[i, j]] != AICoefficient && Coefficient[Board.WIN_STATE[i, j]] != HumanCoefficient )
+                        pos = Board.WIN_STATE[i, j];
+                }
+                if ( count == 2 && pos != -1 )
+                {
+                    Coefficient[pos] = 1000;
+                }
+                count = 0;
+            }
+        }
+
+        public override void Reset ()
+        {
+            Coefficient = new int[] {
+                 1,   0,   1,
+                 0,   100, 0,
+                 1,   0,   1
+            };
+        }
+
+        int _MaxValue ( List<int> freeCell )
+        {
+            int maxPos = -1, maxValue = int.MinValue;
+            foreach ( int cell in freeCell )
+            {
+                if ( maxValue < Coefficient[cell] )
+                {
+                    maxValue = Coefficient[cell];
+                    maxPos = cell;
+                }
+            }
+
+            return maxPos;
+        }
+    }
+}
